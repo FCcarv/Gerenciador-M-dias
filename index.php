bb120919ad422b0fddcb75956d56972d88d308f8
francisco <kiko.fccarvalho@gmail.com>
2017-11-13 11:08:43 -0200

    começando Proj Gerenciando Mídias

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/style.css"/>
+        
+        <title></title>
+    </head>
+    <body>
+        <div class="wrap">
+            <form action="" method="post" enctype="multipart/form-data">
+                <textarea name="post"></textarea>
+                <input type="submit"  value="Cadastrar"/>
+            </form>
+      <div/>
+
+    </body>
+        <script src="js/jquery-migrate-3.0.0.min.js"></script>
+        <script src="tinymce/tinymce.min.js"></script>
+        <script src="js/tinymce.init.js"></script>
+</html>
